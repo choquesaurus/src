2fda3b37066ecde8ceaf4693e28a2168b1f1dbad
Daniel Choquecahuana <54044405+WasauskyOK@users.noreply.github.com>
2020-07-02 19:09:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+if(process.env.NODE_ENV != 'production'){
+	require('dotenv').config()
+}
 require('@babel/polyfill');
 require('@babel/register')({
     /*extends:'./.babelrc',*/
     extensions: [".es6", ".es", ".jsx", ".js", ".mjs"],
     ignore:[/node_modules/]
 });
-require(`./handler/${proccess.env.handler}`);
-
-
+require(`./handler/${process.env.handler}`);
