ebce8b5bfb54d57890661be034e43c1293d046ca
Daniel Choquecahuana <54044405+choquesaurus@users.noreply.github.com>
2021-08-12 01:04:52 -0500

    Update index.js

--- functions/express/index.js
+++ functions/express/index.js
@@ -7,6 +7,7 @@ import morgan from "morgan";
 import rutas from "./routes/index";
 import "./connection/index";
 class Aplication {
+  PORT_RANDOM = process.env.PORT ||5600;
   constructor() {
     this.app = express();
     this.config();
@@ -29,8 +30,8 @@ class Aplication {
     this.app.use("/", rutas);
   }
   start() {
-    this.app.listen(process.env.PORT || 5000 || 5001 || 5002 || 5420, () => {
-      console.log(`Run server in  http://localhost:5000`);
+    this.app.listen(this.PORT_RANDOM, () => {
+      console.log(`Run server in  http://localhost:${this.PORT_RANDOM} :)`);
     });
   }
 }
